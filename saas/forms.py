e5c2de8e1ab7715db6c66fabe37d37544c5934ed
Mark Ahlemeier <markahlemeier@marks-mbp.mobile.asu.edu>
2014-04-19 15:00:22 -0700

    deleted picture field

--- saas/forms.py
+++ saas/forms.py
@@ -12,5 +12,5 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ('website', 'picture')
+        fields = ('website',)
 
